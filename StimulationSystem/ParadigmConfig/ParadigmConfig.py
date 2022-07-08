e10eec75808ea86409b0dfcf769544327a9729da
Daibo <59006279+DayBright-David@users.noreply.github.com>
2022-07-08 10:30:56 +0800

    Add files via upload

---
+++ StimulationSystem/ParadigmConfig/ParadigmConfig.py
@@ -0,0 +1,8 @@
+class ParadigmConfig:
+    def __init__(self):
+        self.paradigm_name = None
+        self.save_time = None
+        self.background_frame_path = None
+        self.stimulation_frames_path = []
+        self.stimTargetRectSet = []
+        self.stringPositions = []
